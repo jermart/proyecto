224fbbfb5916c18fdff677867aecb53452f4c1ee
jermart <jermart23@gmail.com>
2021-11-09 21:50:56 -0500

    avance1
    se instalo npm i  axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React, { Component } from "react";
 import Banner from "./components/Banner";
 import Explanation from "./components/Explanation";
 import Presentation from "./components/Presentation";
 import Stickyfooter from "./componets/Stickyfooter";
 import Leyaut from "./components/Leyaut";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
